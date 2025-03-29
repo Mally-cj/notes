48c119fe2e576afb281e5c2b59f5b0368c8b2386
mally <51468122+Mally-cj@users.noreply.github.com>
2025-03-29 16:26:33 +0800

    update

--- .vuepress/navbar.ts
+++ .vuepress/navbar.ts
@@ -2,24 +2,8 @@ import {NavItem} from "vuepress/config";
 
 export default [
     {
-        text: "计算机网络",
-        link: '/计算机网络/'
-    },
-    {
-        text: "云原生",
-        link: '/云原生/'
-    },
-    {
-        text: "Devops",
-        link: '/Devops/'
-    },
-    {
-        text: "操作系统",
-        link: '/操作系统/'
-    },
-    {
-        text: "Golang",
-        link: '/Golang/'
+        text: "大模型",
+        link: '/大模型/'
     },
     {
         text: "关于作者",
